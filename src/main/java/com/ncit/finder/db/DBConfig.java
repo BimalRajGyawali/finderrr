1b9328b89c3df3c51db0009f5644784318c8080d
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-06-02 23:58:05 +0545

    java downgraded to 1.8

--- src/main/java/com/ncit/finder/db/DBConfig.java
+++ src/main/java/com/ncit/finder/db/DBConfig.java
@@ -11,12 +11,17 @@ public class DBConfig {
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-            String host = "ec2-107-20-153-39.compute-1.amazonaws.com";
-            String database = "d7nsl19ehlcnh5";
-            String username = "hrpvddnprmsjod";
-            String port = "5432";
-            String password = "201123a3c686a22eb14f50e256d81c710457a236b249ebb57665a9ba5b18d3bc";
+            // String host = "ec2-107-20-153-39.compute-1.amazonaws.com";
+            // String database = "d7nsl19ehlcnh5";
+            // String username = "hrpvddnprmsjod";
+            // String port = "5432";
+            // String password = "201123a3c686a22eb14f50e256d81c710457a236b249ebb57665a9ba5b18d3bc";
 
+            String host = "localhost";
+            String database = "postgres";
+            String username = "postgres";
+            String port = "5432";
+            String password = "root";
             String url = "jdbc:postgresql://"+host+":"+port+"/"+database;
 
             dataSourceBuilder.url(url);
