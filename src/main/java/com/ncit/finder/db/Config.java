a63938be5765bdcf37467637d76759c7be4947ee
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-07-17 16:06:24 +0545

    adds database configurations

---
+++ src/main/java/com/ncit/finder/db/Config.java
@@ -0,0 +1,29 @@
+package com.ncit.finder.db;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+
+        String host = "localhost";
+        String database = "finder";
+        String username = "postgres";
+        String port = "5432";
+        String password = "password";
+
+
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
+
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+}
