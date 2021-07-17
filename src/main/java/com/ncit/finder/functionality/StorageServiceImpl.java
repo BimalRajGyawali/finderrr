fcb078a946f96e1b78fc1a79a1adffe12549e4d6
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-07-17 17:30:41 +0545

    refactors to java 8 file copy

--- src/main/java/com/ncit/finder/functionality/StorageServiceImpl.java
+++ src/main/java/com/ncit/finder/functionality/StorageServiceImpl.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.stream.Stream;
 
 import com.ncit.finder.db.Response;
@@ -18,22 +18,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class StorageServiceImpl implements StorageService {
 
 
-	@Override
-	public void store(MultipartFile file,String newName) {
-		int c;
-		InputStream f;
-		try {
-			String uploadDir = "src/main/webapp/resources/uploads/";
-		
-			f = file.getInputStream();
-			OutputStream outputStream=new FileOutputStream(new java.io.File(uploadDir+"/"+newName));
-			
-			while((c=f.read())!=-1) {
-				outputStream.write(c);
-			}
-			
-		} catch(Exception ex){
-			ex.printStackTrace();
-		}
-	}
+    @Override
+    public void store(MultipartFile file, String newName) {
+        String uploadPath = "src/main/webapp/resources/uploads/" + newName;
+        try {
+            Files.copy(file.getInputStream(), Paths.get(uploadPath), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
